bbf410b48e6478d45995757713a77aae21e520e7
Shraddha Mehta <58872789+18dce057@users.noreply.github.com>
2021-12-22 14:08:59 +0530

    Add files via upload

---
+++ Html/js/service-pro.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    var scroll_start = 0;
+    var startchange = $('#navbar-service-page');
+    var offset = startchange.offset();
+    $(document).scroll(function () {
+        scroll_start = $(this).scrollTop();
+        if (scroll_start > offset.top) {
+            $('#nav').css({ "background-color": "rgba(82, 82, 82, 0.9)" });
+            $('#navlogo').css({ "height": "54px", "width": "73px" })
+        } else {
+            $('#nav').css('background-color', 'transparent');
+            $('#navlogo').css({ "height": "130px", "width": "175px" });
+        }
+    });
+});
