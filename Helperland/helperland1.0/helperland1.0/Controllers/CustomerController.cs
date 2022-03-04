ad6348f783bdabf4f8a0735eb396d4c3e31c3e1a
18dce057 <18dce057@charusat.edu.in>
2022-03-04 22:24:49 +0530

    submission

---
+++ Helperland/helperland1.0/helperland1.0/Controllers/CustomerController.cs
@@ -0,0 +1,337 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using helperland1._0.Models;
+using helperland1._0.Models.Data;
+using helperland1._0.ViewModel;
+
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace helperland1._0.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        private readonly HelperlandContext _db;
+
+        public CustomerController(HelperlandContext db)
+        {
+            _db = db;
+        }
+        public IActionResult CustomerDashboard()
+        {
+
+            
+
+
+            if (HttpContext.Session.GetInt32("userId") != null)
+            {
+                var id = HttpContext.Session.GetInt32("userId");
+                 User user = _db.Users.Find(id);
+                ViewBag.Name = user.FirstName;
+                ViewBag.UserType = user.UserTypeId;
+                if (user.UserTypeId == 0)
+                {
+                    return PartialView();
+                }
+            }
+            else if (Request.Cookies["userId"] != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.UserId == Convert.ToInt32(Request.Cookies["userId"]));
+                ViewBag.Name = user.FirstName;
+                ViewBag.UserType = user.UserTypeId;
+                if (user.UserTypeId == 0)
+                {
+                    return PartialView();
+                }
+            }
+            return RedirectToAction("Index", "Public");
+
+
+        }
+
+        public IActionResult BookService()
+        {
+            if (HttpContext.Session.GetInt32("userId") != null)
+            {
+                var id = HttpContext.Session.GetInt32("userId");
+                User user = _db.Users.Find(id);
+                ViewBag.Name = user.FirstName;
+                ViewBag.UserType = user.UserTypeId;
+                if (user.UserTypeId == 0)
+                {
+                    return PartialView();
+                }
+            }
+            else if (Request.Cookies["userId"] != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.UserId == Convert.ToInt32(Request.Cookies["userId"]));
+                ViewBag.Name = user.FirstName;
+                ViewBag.UserType = user.UserTypeId;
+                if (user.UserTypeId == 0)
+                {
+                    return PartialView();
+                }
+            }
+            TempData["add"] = "alert show";
+            TempData["fail"] = "Please Login to book service";
+            return RedirectToAction("Index", "Public", new { loginFail = "true" });
+
+
+        }
+
+        [HttpPost]
+        public IActionResult  ValidPostalCode(PostalCode obj)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var list = _db.Users.Where(x => (x.ZipCode == obj.postalcode) && (x.UserTypeId == 1)).ToList();
+
+
+                if (list.Count() > 0)
+                {
+
+
+                    return Ok(Json("true"));
+                }
+               // TempData["wrongZipCode"] = "Postal code you have entered is not valid.";
+                return Ok(Json("false"));
+            }
+            else
+            {
+                return Ok(Json("Invalid"));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ScheduleService(ScheduleService schedule)
+        {
+           
+             if (ModelState.IsValid)
+             {
+
+                
+                 return Ok(Json("true"));
+
+
+             }
+             else
+             {
+               
+                return Ok(Json("false"));
+             }
+
+         
+
+        }
+
+
+        [HttpGet]
+        public IActionResult DetailsService(PostalCode obj)
+        {
+
+            int Id = -1;
+
+            List<Address> Addresses = new List<Address>();
+            if(HttpContext.Session.GetInt32("userId") != null)
+            {
+                Id = (int)HttpContext.Session.GetInt32("userId");
+            }
+            else if(Request.Cookies["userId"] != null)
+            {
+                Id = int.Parse(Request.Cookies["userId"]);
+             
+            }
+            
+           
+            string postalcode = obj.postalcode;
+            Console.WriteLine(obj.postalcode);
+            var table = _db.UserAddresses.Where(x => x.UserId == Id && x.PostalCode == postalcode).ToList();
+            Console.WriteLine(table.ToString());
+
+            foreach (var add in table)
+            {
+                Console.WriteLine("1");
+                Address useradd = new Address();
+                useradd.AddressId = add.AddressId;
+                useradd.AddressLine1 = add.AddressLine1;
+                useradd.AddressLine2 = add.AddressLine2;
+                useradd.City = add.City;
+                useradd.PostalCode = add.PostalCode;
+                useradd.Mobile = add.Mobile;
+                useradd.isDefault = add.IsDefault;
+                
+                Addresses.Add(useradd);
+            }
+            Console.WriteLine("2");
+
+            return new JsonResult(Addresses);
+        }
+
+
+        [HttpPost]
+        public IActionResult AddNewAddress(UserAddress useradd)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+
+                Console.WriteLine("Inside Addnew address 1");
+                int Id = -1;
+
+
+                if (HttpContext.Session.GetInt32("userId") != null)
+                {
+                    Id = (int)HttpContext.Session.GetInt32("userId");
+                }
+                else if (Request.Cookies["userId"] != null)
+                {
+                    Id = int.Parse(Request.Cookies["userId"]);
+
+                }
+                Console.WriteLine("Inside Addnew address 2");
+                Console.WriteLine(Id);
+
+                useradd.UserId = Id;
+                useradd.IsDefault = false;
+                useradd.IsDeleted = false;
+                User user = _db.Users.Where(x => x.UserId == Id).FirstOrDefault();
+                useradd.Email = user.Email;
+                var result = _db.UserAddresses.Add(useradd);
+                Console.WriteLine("Inside Addnew address 3");
+                _db.SaveChanges();
+
+                Console.WriteLine("Inside Addnew address 4");
+                if (result != null)
+                {
+                    return Ok(Json("true"));
+                }
+
+                return Ok(Json("false"));
+
+            }
+            return View();
+        }
+
+
+
+
+        public ActionResult CompleteBooking(CompleteBooking complete)
+        {
+            int Id = -1;
+
+          
+            if (HttpContext.Session.GetInt32("userId") != null)
+            {
+                Id = (int)HttpContext.Session.GetInt32("userId");
+            }
+            else if (Request.Cookies["userId"] != null)
+            {
+                Id = int.Parse(Request.Cookies["userId"]);
+
+            }
+
+
+            ServiceRequest add = new ServiceRequest();
+            add.UserId = Id;
+            add.ServiceId = Id;
+            add.ServiceStartDate = complete.ServiceStartDate;
+            add.ServiceHours = (double)complete.ServiceHours;
+            add.ZipCode = complete.PostalCode;
+            add.ServiceHourlyRate = 25;
+            add.ExtraHours = complete.ExtraHours;
+            add.SubTotal = (decimal)complete.SubTotal;
+            add.TotalCost = (decimal)complete.TotalCost;
+            add.Comments = complete.Comments;
+            add.PaymentDue = false;
+            add.PaymentDone = true;
+            add.HasPets = complete.HasPets;
+            add.CreatedDate = DateTime.Now;
+            add.ModifiedDate = DateTime.Now;
+            add.HasIssue = false;
+
+            var result = _db.ServiceRequests.Add(add);
+            _db.SaveChanges();
+
+            UserAddress useraddr = _db.UserAddresses.Where(x => x.AddressId == complete.AddressId).FirstOrDefault();
+
+            ServiceRequestAddress srAddr = new ServiceRequestAddress();
+            srAddr.AddressLine1 = useraddr.AddressLine1;
+            srAddr.AddressLine2 = useraddr.AddressLine2;
+            srAddr.City = useraddr.City;
+            srAddr.Email = useraddr.Email;
+            srAddr.Mobile = useraddr.Mobile;
+            srAddr.PostalCode = useraddr.PostalCode;
+            srAddr.ServiceRequestId = result.Entity.ServiceRequestId;
+            srAddr.State = useraddr.State;
+
+            var srAddrResult = _db.ServiceRequestAddresses.Add(srAddr);
+            _db.SaveChanges();
+
+            if (complete.Cabinet == true)
+            {
+                ServiceRequestExtra srExtra = new ServiceRequestExtra();
+                srExtra.ServiceRequestId = result.Entity.ServiceRequestId;
+                srExtra.ServiceExtraId = 1;
+                _db.ServiceRequestExtras.Add(srExtra);
+                _db.SaveChanges();
+            }
+            if (complete.Fridge == true)
+            {
+                ServiceRequestExtra srExtra = new ServiceRequestExtra();
+                srExtra.ServiceRequestId = result.Entity.ServiceRequestId;
+                srExtra.ServiceExtraId = 2;
+                _db.ServiceRequestExtras.Add(srExtra);
+                _db.SaveChanges();
+            }
+            if (complete.Oven == true)
+            {
+                ServiceRequestExtra srExtra = new ServiceRequestExtra();
+                srExtra.ServiceRequestId = result.Entity.ServiceRequestId;
+                srExtra.ServiceExtraId = 3;
+                _db.ServiceRequestExtras.Add(srExtra);
+                _db.SaveChanges();
+            }
+            if (complete.Laundry == true)
+            {
+                ServiceRequestExtra srExtra = new ServiceRequestExtra();
+                srExtra.ServiceRequestId = result.Entity.ServiceRequestId;
+                srExtra.ServiceExtraId = 4;
+                _db.ServiceRequestExtras.Add(srExtra);
+                _db.SaveChanges();
+            }
+            if (complete.Window == true)
+            {
+                ServiceRequestExtra srExtra = new ServiceRequestExtra();
+                srExtra.ServiceRequestId = result.Entity.ServiceRequestId;
+                srExtra.ServiceExtraId = 5;
+                _db.ServiceRequestExtras.Add(srExtra);
+                _db.SaveChanges();
+            }
+           
+           
+
+            if (result != null && srAddrResult != null)
+            {
+                return Ok(Json(result.Entity.ServiceRequestId));
+            }
+
+            return Ok(Json("false"));
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Helperland/helperland1.0/helperland1.0/Controllers/UserManagementController.cs
@@ -0,0 +1,243 @@
+﻿using helperland1._0.Models;
+using helperland1._0.Models.Data;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using System;
+using System.Linq;
+
+
+namespace helperland1._0.Controllers
+{
+    public class UserManagementController : Controller
+    {
+
+        private readonly HelperlandContext _db;
+
+        public UserManagementController(HelperlandContext db)
+        {
+            _db = db;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginUser user)
+        {
+            if (ModelState.IsValid)
+            {
+
+                string password = _db.Users.FirstOrDefault(x => x.Email == user.username).Password;
+
+                bool pass = BCrypt.Net.BCrypt.Verify(user.password, password);
+                if (_db.Users.Where(x => x.Email == user.username && pass).Count() > 0)
+                {
+
+                     var U = _db.Users.FirstOrDefault(x => x.Email == user.username);
+
+                    Console.WriteLine("1");
+
+                    if (user.remember == true)
+                    {
+                        CookieOptions cookieRemember = new CookieOptions();
+                        cookieRemember.Expires = DateTime.Now.AddSeconds(604800);
+                        Response.Cookies.Append("userId", Convert.ToString(U.UserId), cookieRemember);
+                    }
+                   
+
+                    HttpContext.Session.SetInt32("userId", U.UserId);
+
+                 
+
+                    if (U.UserTypeId == 0)
+                    {
+                        return RedirectToAction("CustomerDashboard", "Customer");
+                    }
+                  /* else if (user.UserTypeId == 2)
+                    {
+                        return RedirectToAction("SPUpcomingService", "ServicePro");
+                    }
+                    else if (user.UserTypeId == 3)
+                    {
+                        return RedirectToAction("ServiceRequest", "Admin");
+                    }*/
+
+                    return RedirectToAction("CustomerDashboard", "Customer");
+                }
+                else
+                {
+                    TempData["add"] = "alert show";
+                    TempData["fail"] = "username and password are invalid";
+                    return RedirectToAction("Index", "Public", new { loginFail = "true" });
+
+                }
+            }
+
+            TempData["add"] = "alert show";
+            TempData["fail"] = "username and password are required";
+            return RedirectToAction("Index", "Public", new { loginModal = "true" });
+
+
+
+
+
+        }
+
+        public IActionResult BecomeAProvider()
+        {
+            return View();
+        }
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult BecomeAProvider(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if ((_db.Users.Where(x => x.Email == user.Email).Count() == 0 && _db.Users.Where(x => x.Mobile == user.Mobile).Count() == 0))
+                {
+
+                    user.CreatedDate = DateTime.Now;
+                    user.ModifiedDate = DateTime.Now;
+                    user.IsRegisteredUser = true;
+                    user.ModifiedBy = 123;
+                    user.UserTypeId = 1;
+                    user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+
+                    TempData["add"] = "alert show";
+                    TempData["msg"] = "Account created, Wait for admin approval";
+                    return RedirectToAction("Index", "Public");
+
+
+                }
+                else
+                {
+                    TempData["add"] = "alert show";
+                    TempData["msg"] = "Username Exits!";
+                    return View();
+
+
+                }
+            }
+            return View();
+
+        }
+
+        public IActionResult CustomerSignUp()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult CustomerSignUp(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if ((_db.Users.Where(x => x.Email == user.Email).Count() == 0 && _db.Users.Where(x => x.Mobile == user.Mobile).Count() == 0))
+                {
+
+                    user.CreatedDate = DateTime.Now;
+                    user.ModifiedDate = DateTime.Now;
+                    user.IsRegisteredUser = true;
+                    user.ModifiedBy = 123;
+                    user.UserTypeId = 0;
+                    user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+                    _db.Users.Add(user);
+                    _db.SaveChanges();
+
+                    TempData["add"] = "alert show";
+                    TempData["msg"] = "Account created, Please Login";
+                    return RedirectToAction("Index", "Public", new { loginModal = "true" });
+
+
+                }
+                else
+                {
+                    TempData["add"] = "alert show";
+                    TempData["msg"] = "Username Exits!";
+                    return View();
+
+
+                }
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult SendMail(string email)
+        {
+
+            if (email != null)
+            {
+                var user = _db.Users.FirstOrDefault(x => x.Email == email);
+
+
+                MimeMessage message = new MimeMessage();
+
+                MailboxAddress from = new MailboxAddress("Helperland",
+                "vedantjotangiya@gmail.com");
+                message.From.Add(from);
+
+                MailboxAddress to = new MailboxAddress(user.FirstName, email);
+                message.To.Add(to);
+
+                message.Subject = "Reset Password";
+
+                BodyBuilder bodyBuilder = new BodyBuilder();
+                bodyBuilder.HtmlBody = "<h1>Reset your password by click below link</h1>" +
+                    "<a href='" + Url.Action("ResetPassword", "UserManagement", new { userId = user.UserId }, "https") + "'>Reset Password</a>";
+
+
+                message.Body = bodyBuilder.ToMessageBody();
+
+                SmtpClient client = new SmtpClient();
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("vedantjotangiya@gmail.com", "Vedantjot@123");
+                client.Send(message);
+                client.Disconnect(true);
+                client.Dispose();
+                return RedirectToAction("Index", "Public", new { mailSended = "true" });
+            }
+            return RedirectToAction("Index", "Public");
+        }
+
+
+        [HttpGet]
+        public IActionResult ResetPassword(int userId)
+        {
+            TempData["id"] = userId;
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult ResetPassword(ResetPassword rp)
+        {
+            rp.password = BCrypt.Net.BCrypt.HashPassword(rp.password);
+            var user = new User() { UserId = rp.userId, Password = rp.password, ModifiedDate = DateTime.Now };
+            _db.Users.Attach(user);
+            _db.Entry(user).Property(x => x.Password).IsModified = true;
+            _db.Entry(user).Property(x => x.Password).IsModified = true;
+            _db.SaveChanges();
+
+
+            return RedirectToAction("Index", "Public", new { loginModal = "true" });
+        }
+
+        public IActionResult logout()
+        {
+            HttpContext.Session.Clear();
+
+            Response.Cookies.Delete("userId");
+            return RedirectToAction("Index", "Public", new { logoutModal = "true" });
+        }
+
+    }
+}
+
+
